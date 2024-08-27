cf3eba1c326b4c8735113c32cb02da744e8a8a6d
Mahady Hasan Fahim <fmahady01@gmail.com>
2024-08-27 07:47:33 +0600

    JWT DONE

--- src/main/java/com/fahim/security_jwt/security/MyUserController.java
+++ src/main/java/com/fahim/security_jwt/security/MyUserController.java
@@ -1,6 +1,11 @@
 package com.fahim.security_jwt.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +15,19 @@ import java.util.List;
 
 @RestController
 public class MyUserController {
+
+
     @Autowired
     private MyUserService myUserService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private MyUserdetailsService myUserdetailsService;
+
+
+
 
     @PostMapping("/registration")
     public MyUser createUser(@RequestBody final MyUser myUser) {
@@ -29,4 +45,22 @@ public class MyUserController {
         return myUserService.findAll();
     }
 
-}
+
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody LoginForm loginForm) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginForm.username(), loginForm.password()
+        ));
+
+        if (authentication.isAuthenticated()) {
+
+            return jwtService.generateToken(myUserdetailsService.loadUserByUsername(loginForm.username()));
+        } else {
+
+            throw new UsernameNotFoundException("Invalid credentials");
+        }
+    }
+
+    }
+
+
--- src/main/java/com/fahim/security_jwt/security/SecurityConfuguration.java
+++ src/main/java/com/fahim/security_jwt/security/SecurityConfuguration.java
@@ -3,15 +3,19 @@ package com.fahim.security_jwt.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -23,6 +27,9 @@ public class SecurityConfuguration {
     @Autowired
     private MyUserdetailsService myUserdetailsService;
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -32,17 +39,21 @@ public class SecurityConfuguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> {
-                    authorizeRequests
-                            .requestMatchers("/registration", "/login", "/", "/logout").permitAll()
-                            .requestMatchers("/admin/**").hasRole("ADMIN")
-                            .requestMatchers("/user/**").hasRole("USER")
-                            .anyRequest().authenticated();
+
+                    authorizeRequests.requestMatchers("/registration", "/login", "/", "/logout","/authenticate").permitAll();
+                    authorizeRequests.requestMatchers("/admin/**").hasRole("ADMIN");
+                    authorizeRequests.requestMatchers("/user/**").hasRole("USER");
+                    authorizeRequests.anyRequest().authenticated();
                 })
-                .httpBasic(withDefaults()) //// Explicitly configure Basic Authentication
-                .formLogin(formLogin -> formLogin
-                        .successHandler(new AuthenticationSuccessHandler())
-                        .permitAll()
-                )
+
+//                .httpBasic(withDefaults()) //// Explicitly configure Basic Authentication
+//                .formLogin(formLogin -> formLogin
+//                        .successHandler(new AuthenticationSuccessHandler())
+//                        .permitAll()
+//                )
+//                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+
                 .build();
     }
 
@@ -59,5 +70,9 @@ public class SecurityConfuguration {
         return provider;
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        return new ProviderManager(authenticationProvider());
+    }
 
 }
